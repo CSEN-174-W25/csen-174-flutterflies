97a15ddb0718133c79c9cc0c320c01d9df6126aa
rose413 <irenechang949@gmail.com>
2025-02-25 15:23:33 -0800

    Updated group functionalities

--- scusocial/lib/pages/event_page.dart
+++ scusocial/lib/pages/event_page.dart
@@ -10,7 +10,6 @@ import '../services/firestore_service.dart';
 import '../pages/manage_friends.dart';
 import 'group/create_group.dart';
 import 'group/search_group.dart';
-import 'group/join_group.dart';
 
 class EventPage extends StatelessWidget {
   final User user;
