effbeabd8943722517a17eea0d40842c41b687f7
rose413 <irenechang949@gmail.com>
2025-02-25 15:44:46 -0800

    Create gorup_page.dart

---
+++ scusocial/lib/pages/group/group_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GroupPage extends StatefulWidget {
+  @override
+  _GroupPageState createState() => _GroupPageState();
+}
+
